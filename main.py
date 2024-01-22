157c452a7b42649396a36c1f4eebe550c995e32c
chechetov <dr.dan79@gmail.com>
2024-01-22 16:01:25 +0100

    add more query parameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 class BlogType(str, Enum):
   short = 'short'
@@ -25,6 +26,10 @@ def index2():
 @app.get('/blog/all')
 def get_all_blogs(page = 1, page_size = 10):
   return {'message': f'All {page_size} blogs on page {page}'}
+
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+  return {'message': f'blog_id: {id}, comment_id: {comment_id}, valid: {valid}, username: {username}'}
 
 @app.get('/blog/{type}')
 def get_blog_by_type(type: BlogType):
