2c54fa8d047def9b7a996e55ac8327fbbdb5e326
chechetov <dr.dan79@gmail.com>
2024-01-23 13:27:00 +0100

    move get requests to blog_get

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from fastapi import FastAPI, status, Response
-from enum import Enum
-from typing import Optional
-
-class BlogType(str, Enum):
-  short = 'short'
-  story = 'story'
-  howto = 'howto'
+from fastapi import FastAPI
+from router import blog_get
 
 app = FastAPI()
+app.include_router(blog_get.router)
 
 @app.get('/hello')
 def index():
@@ -17,48 +12,4 @@ def index():
 def index2():
   return 'Hi'
 
-# Order is important!
-# If id: int on top - it caches everything
-#@app.get('/blog/all')
-#def get_all_blogs():
-#  return {'message':'All blogs returned'}
 
-@app.get('/blog/all',
-  tags = ['blog','comment'],
-  summary='Retrieve all blogs',
-  description='Api call to get all blogs',
-  response_description='The list of all available blogs'
-  )
-def get_all_blogs(page = 1, page_size = 10):
-  return {'message': f'All {page_size} blogs on page {page}'}
-
-@app.get('/blog/{id}/comments/{comment_id}',
-  tags = ['blog'])
-def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
-  """
-  Simulates retrieving a comment
-
-  - **id** mandatory path parameter
-  - **comment_id** mandatory path parameter
-  - **valid** optional query parameter
-  - **username** optional query parameter
-  """
-  return {'message': f'blog_id: {id}, comment_id: {comment_id}, valid: {valid}, username: {username}'}
-
-# I can not have /blog/{type} and /blog/{id} at the same time!
-# Basically /blog/{var}
-@app.get('/blog/type/{type}',
-  tags = ['blog'])
-def get_blog_by_type(type: BlogType):
-  return {'message': "Returning blog of " + type}
-
-@app.get('/blog/{id}', 
-  tags = ['blog'],
-  status_code = status.HTTP_404_NOT_FOUND)
-def get_blog(id: int, response: Response):
-  if id > 5:
-    response.status_code = status.HTTP_404_NOT_FOUND
-    return {'message': f'hello {id}'}
-  else:
-    response.status_code = status.HTTP_200_OK
-    return {'message': "Blog with id : " + str(id)}
--- router/blog_get.py
+++ router/blog_get.py
@@ -1,7 +1,59 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Response
+from enum import Enum
+from typing import Optional
+
+class BlogType(str, Enum):
+  short = 'short'
+  story = 'story'
+  howto = 'howto'
 
 router = APIRouter(
 	prefix='/blog',
 	tags=['blog']
 	)
 
+# Order is important!
+# If id: int on top - it caches everything
+#@router.get('/blog/all')
+#def get_all_blogs():
+#  return {'message':'All blogs returned'}
+
+@router.get('/blog/all',
+  tags = ['blog','comment'],
+  summary='Retrieve all blogs',
+  description='Api call to get all blogs',
+  response_description='The list of all available blogs'
+  )
+def get_all_blogs(page = 1, page_size = 10):
+  return {'message': f'All {page_size} blogs on page {page}'}
+
+@router.get('/blog/{id}/comments/{comment_id}',
+  tags = ['blog'])
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+  """
+  Simulates retrieving a comment
+
+  - **id** mandatory path parameter
+  - **comment_id** mandatory path parameter
+  - **valid** optional query parameter
+  - **username** optional query parameter
+  """
+  return {'message': f'blog_id: {id}, comment_id: {comment_id}, valid: {valid}, username: {username}'}
+
+# I can not have /blog/{type} and /blog/{id} at the same time!
+# Basically /blog/{var}
+@router.get('/blog/type/{type}',
+  tags = ['blog'])
+def get_blog_by_type(type: BlogType):
+  return {'message': "Returning blog of " + type}
+
+@router.get('/blog/{id}', 
+  tags = ['blog'],
+  status_code = status.HTTP_404_NOT_FOUND)
+def get_blog(id: int, response: Response):
+  if id > 5:
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return {'message': f'hello {id}'}
+  else:
+    response.status_code = status.HTTP_200_OK
+    return {'message': "Blog with id : " + str(id)}
