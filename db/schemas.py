35c5316f8f86fee84268d512edbe9f384bb7d6f7
chechetov <dr.dan79@gmail.com>
2024-01-26 14:36:11 +0100

    add article to schemas.py

--- db/schemas.py
+++ db/schemas.py
@@ -1,4 +1,14 @@
+from typing import List
 from pydantic import BaseModel
+
+# Article schema? model?
+# Article inside UserDisplay (only for UserDisplay)
+class Article(BaseModel):
+  title: str
+  content: str
+  published: bool
+  class Config():
+    orm_mode=True
 
 # Data we receive from user
 class UserBase(BaseModel):
@@ -6,9 +16,33 @@ class UserBase(BaseModel):
   email: str
   password: str
 
-# Data we provide to user
+# Data we display to user
 class UserDisplay(BaseModel):
   username: str
   email: str
+  items: List[Article] = []
   class Config():
     orm_mode=True
+
+class ArticleBase(BaseModel):
+  title: str
+  content: str
+  published: bool
+  creator_id: int
+
+# User inside ArticleDisplay
+class User(BaseModel):
+  id: int
+  username : str
+  class Config():
+    orm_mode=True
+
+# Data we display to user
+class ArticleDisplay(BaseModel):
+  title: str
+  content: str
+  published: bool
+  user: User
+  class Config():
+    orm_mode=True
+
