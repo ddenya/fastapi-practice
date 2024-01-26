42264fbf04c6577d918a141c3ad8d4c8bf8004c0
chechetov <dr.dan79@gmail.com>
2024-01-26 18:28:19 +0100

    add get_all, update, delete to db_article.py

--- db/db_article.py
+++ db/db_article.py
@@ -1,14 +1,23 @@
 from sqlalchemy.orm.session import Session
-from schemas import ArticleBase
+from db.schemas import ArticleBase
 from models.models import DbArticle
 
+# Get one article
 def get_article(id: int, db: Session):
   try:
-    article = db.query(DbArticle).filter(DbArticle.id == id).first()
-    return article
+    return db.query(DbArticle).filter(DbArticle.id == id).first()
   except Exception as e:
-    return {'result': False}
+    return {}
+  
+# Get all articles
+def get_all_articles(db: Session):
+  try:
+    articles = db.query(DbArticle).all()
+    return articles
+  except Exception as e:
+    return {}
 
+# Create article
 def create_article(request: ArticleBase, db: Session):
   try:
     new_article = DbArticle(
@@ -24,4 +33,25 @@ def create_article(request: ArticleBase, db: Session):
     return new_article
   except Exception as e:
     return {'result': False}
+  
+# Update article
+def update_article(id: int, request: ArticleBase, db: Session):
+  try:
+    article = get_article(id, db)
+    article.title = request.title
+    article.content = request.content
+    article.published = request.published
+    db.commit()
+    return article
+  except Exception as e:
+    return {'result': False}
 
+# Delete article
+def delete_article(id: int, db: Session):
+  try:
+    article = get_article(id, db)
+    db.delete(article)
+    db.commit()
+    return {'result': True}
+  except Exception as e:
+    return {'result': False}
